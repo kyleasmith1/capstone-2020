abda4195ce3f2f29c1a90129958bae6cab2044b0
kyleasmith1 <kysmith152@gmail.com>
2020-07-22 19:35:35 +0000

    Change index.html to send a token, create auth servlet

--- capstone/src/main/webapp/script.js
+++ capstone/src/main/webapp/script.js
@@ -29,10 +29,24 @@ function addRandomGreeting() {
 
 function onSignIn(googleUser) {
   var profile = googleUser.getBasicProfile();
+  var id_token = googleUser.getAuthResponse().id_token;
+  console.log(profile);
+  console.log(id_token);
+
+  var xhr = new XMLHttpRequest();
+  xhr.open('POST', '/auth');
+  xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+  xhr.onload = function() {
+    console.log('Signed in as: ' + xhr.responseText);
+  };
+  xhr.send('idtoken=' + id_token);
+  
+  /*
   console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
   console.log('Name: ' + profile.getName());
   console.log('Image URL: ' + profile.getImageUrl());
   console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+  */
 }
 
 function myInfo(){
@@ -47,3 +61,12 @@ function myInfo(){
         console.log('Email: ' + profile.getEmail());
     }
 }
+
+function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+        console.log('User signed out.');
+    });
+}
+
+f

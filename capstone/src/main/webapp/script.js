0dcc5e45db854ff56a9de8474ee2753ba1231f96
kyleasmith1 <kysmith152@gmail.com>
2020-07-23 23:19:15 +0000

    Fail debug with Scott

--- capstone/src/main/webapp/script.js
+++ capstone/src/main/webapp/script.js
@@ -76,3 +76,100 @@ function callTestScript(){
             console.log(url)
     });
 }
+
+      // Client ID and API key from the Developer Console
+      var CLIENT_ID = '914921573408-mnpm4t2hsn4mbvjkjho6qrsmbe10lkt6.apps.googleusercontent.com';
+      var API_KEY = 'AIzaSyCibNXSiDMVZCsjXzqZxLkzjV9fSD5CqCk';
+
+      // Array of API discovery doc URLs for APIs used by the quickstart
+      var DISCOVERY_DOCS = ["https://script.googleapis.com/$discovery/rest?version=v1"];
+
+      // Authorization scopes required by the API; multiple scopes can be
+      // included, separated by spaces.
+      var SCOPES = 'https://www.googleapis.com/auth/script.projects';
+
+      var authorizeButton = document.getElementById('authorize_button');
+      var signoutButton = document.getElementById('signout_button');
+
+      /**
+       *  On load, called to load the auth2 library and API client library.
+       */
+      function handleClientLoad() {
+        gapi.load('client:auth2', initClient);
+      }
+
+      /**
+       *  Initializes the API client library and sets up sign-in state
+       *  listeners.
+       */
+      function initClient() {
+        gapi.client.init({
+          apiKey: API_KEY,
+          clientId: CLIENT_ID,
+          discoveryDocs: DISCOVERY_DOCS,
+          scope: SCOPES
+        }).then(function () {
+            callScriptFunction();
+
+        }, function(error) {
+          console.log(JSON.stringify(error, null, 2));
+        });
+      }
+
+
+
+function callScriptFunction() {
+  var scriptId = "MbD62Unoj4HJEpa_WgFlR4ScaIyW1MhlU";
+
+  // Call the Apps Script API run method
+  //   'scriptId' is the URL parameter that states what script to run
+  //   'resource' describes the run request body (with the function name
+  //              to execute)
+  gapi.client.script.scripts.run({
+    'scriptId': scriptId,
+    'resource': {
+      'function': 'doGet'
+    }
+  }).then(function(resp) {
+    var result = resp.result;
+    if (result.error && result.error.status) {
+      // The API encountered a problem before the script
+      // started executing.
+      appendPre('Error calling API:');
+      appendPre(JSON.stringify(result, null, 2));
+    } else if (result.error) {
+      // The API executed, but the script returned an error.
+
+      // Extract the first (and only) set of error details.
+      // The values of this object are the script's 'errorMessage' and
+      // 'errorType', and an array of stack trace elements.
+      var error = result.error.details[0];
+      appendPre('Script error message: ' + error.errorMessage);
+
+      if (error.scriptStackTraceElements) {
+        // There may not be a stacktrace if the script didn't start
+        // executing.
+        appendPre('Script error stacktrace:');
+        for (var i = 0; i < error.scriptStackTraceElements.length; i++) {
+          var trace = error.scriptStackTraceElements[i];
+          appendPre('\t' + trace.function + ':' + trace.lineNumber);
+        }
+      }
+    } else {
+      // The structure of the result will depend upon what the Apps
+      // Script function returns. Here, the function returns an Apps
+      // Script Object with String keys and values, and so the result
+      // is treated as a JavaScript object (folderSet).
+
+      var folderSet = result.response.result;
+      if (Object.keys(folderSet).length == 0) {
+          appendPre('No folders returned!');
+      } else {
+        appendPre('Folders under your root folder:');
+        Object.keys(folderSet).forEach(function(id){
+          appendPre('\t' + folderSet[id] + ' (' + id  + ')');
+        });
+      }
+    }
+  });
+}

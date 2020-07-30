23f36f54e0c3d1c06c44be9cd7fb74e532af08c7
Andrew Pham <0andrewpham0@gmail.com>
2020-07-30 17:41:12 +0000

    Add doGet method

--- capstone/src/main/java/com/google/sps/servlets/FormHandlerServlet.java
+++ capstone/src/main/java/com/google/sps/servlets/FormHandlerServlet.java
@@ -3,7 +3,12 @@ import java.io.IOException;
 import java.io.BufferedReader;
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Query.SortDirection;
 import com.google.appengine.api.datastore.Entity;
+import java.util.List;
+import java.util.ArrayList;
 import com.google.gson.Gson;
 import com.google.sps.data.Form;
 import com.google.sps.data.RequestJsonParser;
@@ -14,6 +19,26 @@ import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/form-handler")
 public class FormHandlerServlet extends HttpServlet {
+
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        List<Form> forms = new ArrayList<>();
+        Form form = null;
+        Query query = new Query ("Form");
+        DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+        PreparedQuery results = datastore.prepare(query);
+    
+        for(Entity entity : results.asIterable()){
+            form = new Form(entity);
+            forms.add(form);
+        }
+    
+        Gson gson = new Gson();
+        String json = gson.toJson(forms);
+        response.setContentType("application/json");
+        response.getWriter().println(json);
+    }
+
 
     @Override
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {

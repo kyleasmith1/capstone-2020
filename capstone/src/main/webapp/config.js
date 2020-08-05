55d0b7a796498080396761a35f79548f4781263b
Kyle Smith <kysmith152@gmail.com>
2020-08-05 18:11:06 +0000

    Untrack files in .gitignore

--- capstone/src/main/webapp/config.js
+++
@@ -1,5 +0,0 @@
-var config = {
-  API_KEY : 'AIzaSyCbowMYfGfEdWI-aWtY2OkrcTwH9ad2iGw',
-  CLIENT_ID : '914921573408-h9jd30r94b1a3kqr5dg2t7bq9v670sbj.apps.googleusercontent.com',
-  PROJECT_ID : 'kyleasmith-step-2020'
-}

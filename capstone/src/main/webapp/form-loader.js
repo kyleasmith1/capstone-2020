adcbdd324c35ab4b1b337d7d209345216405a431
Kyle Smith <kysmith152@gmail.com>
2020-08-07 00:07:24 +0000

    remove script Id

--- capstone/src/main/webapp/form-loader.js
+++ capstone/src/main/webapp/form-loader.js
@@ -1,7 +1,7 @@
 document.body.prepend(dynamicButton);
 
 function callScriptFunction() {
-    var scriptId = "1Z_td2xr1Hq9loDzSdojcCS_3qFwKJR3apBuR2zmcyUVpdhqvfJWyMMYZ";
+    var scriptId = "";
   
     // Call the Apps Script API run method
     //   'scriptId' is the URL parameter that states what script to run

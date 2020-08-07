f9446f930a72869b13d69e18d1f55984bbe17859
Kyle Smith <kysmith152@gmail.com>
2020-08-07 19:00:34 +0000

    Matt's changes: remove nesting, safer error handling

--- capstone/src/main/webapp/script.js
+++ capstone/src/main/webapp/script.js
@@ -16,7 +16,7 @@ function onSignIn(googleUser) {
   var profile = googleUser.getBasicProfile();
   var id_token = googleUser.getAuthResponse().id_token;
   return fetch("/login", {method: "POST", body: id_token}).then((resp) => {
-        if (resp.ok){
+        if (resp?.ok){
             window.location.href = "form.html";
         } else {
             alert("Error has occured");

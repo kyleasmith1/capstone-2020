64cd90f9eee9d377d6cfa97f978d99f29893e5da
Andrew Pham <0andrewpham0@gmail.com>
2020-08-07 17:40:12 +0000

    Fix .gitignore

--- capstone/src/main/webapp/config.js
+++
@@ -1,6 +0,0 @@
-var config = {
-  API_KEY : 'AIzaSyBS13DWNfA-RpO4AQrd4tdUE15-chVDbJc',
-  CLIENT_ID : '1002281613740-gsmtp3tteqgtoformikuglhu8fr388et.apps.googleusercontent.com',
-  PROJECT_ID : 'adpham-step-2020',
-  SCRIPT_ID: "1Z_td2xr1Hq9loDzSdojcCS_3qFwKJR3apBuR2zmcyUVpdhqvfJWyMMYZ"
-}

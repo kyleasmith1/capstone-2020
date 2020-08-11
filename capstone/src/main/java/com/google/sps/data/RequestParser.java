d8f5e2bf0799a6fae8bb6ad908c81c4dac69fb31
Andrew Pham <0andrewpham0@gmail.com>
2020-08-11 22:23:40 +0000

    Add verify helper functions to RequestParser.java

--- capstone/src/main/java/com/google/sps/data/RequestParser.java
+++ capstone/src/main/java/com/google/sps/data/RequestParser.java
@@ -1,10 +1,15 @@
 package com.google.sps.data;
+
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 import java.io.BufferedReader;
 import com.google.gson.Gson;
 import com.google.gson.JsonSyntaxException;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken.Payload;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
+import java.security.GeneralSecurityException;
 
 public class RequestParser {
   
@@ -24,5 +29,20 @@ public class RequestParser {
             reader.close();
         }
         return sb.toString();
+    }
+
+    public static GoogleIdToken verifyTokenFromRequest(HttpServletRequest request) throws IOException, GeneralSecurityException {    
+        GoogleIdToken idToken = tokenVerifier().verify(RequestParser.parseStringFromRequest(request));
+        if (idToken != null){
+            return idToken;
+        } else {
+            throw new GeneralSecurityException();
+        }
+    }
+ 
+    private static GoogleIdTokenVerifier tokenVerifier() {
+        return new GoogleIdTokenVerifier.Builder(new NetHttpTransport(), new JacksonFactory())
+            .setAudience(Collections.singletonList(Config.CLIENT_ID))
+            .build();
     }
 }

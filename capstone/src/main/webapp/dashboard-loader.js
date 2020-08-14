507d665f6d754ffdef5f494ee9e58aa833177b94
Andrew Pham <0andrewpham0@gmail.com>
2020-08-14 03:00:33 +0000

    Remove unneeded user info in request

--- capstone/src/main/webapp/dashboard-loader.js
+++ capstone/src/main/webapp/dashboard-loader.js
@@ -2,16 +2,7 @@ document.body.prepend(dynamicButton);
 window.addEventListener('authorized', getClassrooms);
 
 function createClassroom(subject) {
-    var auth2 = gapi.auth2.getAuthInstance();
-    var name = "";
-    var email = "";
-
-    if (auth2.isSignedIn.get()) {
-        name = auth2.currentUser.get().getBasicProfile().getName();
-        email = auth2.currentUser.get().getBasicProfile().getEmail();
-    }
-
-    var classroomData = JSON.stringify({ "nickname": name, "userId": email, "subject": subject });
+    var classroomData = JSON.stringify({"subject": subject});
     fetch("/dashboard", {method: "POST", headers: new Headers({ID_TOKEN}), body: classroomData}).then((resp) => {
         if (resp.ok){
             getClassrooms();

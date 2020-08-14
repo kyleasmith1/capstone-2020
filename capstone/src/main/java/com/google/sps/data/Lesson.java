ed8046ca74ac49b722427768b10188d3df40bb6a
Andrew Pham <0andrewpham0@gmail.com>
2020-08-14 16:28:05 +0000

    Add Helper functions

--- capstone/src/main/java/com/google/sps/data/Lesson.java
+++ capstone/src/main/java/com/google/sps/data/Lesson.java
@@ -14,17 +14,50 @@ public abstract class Lesson {
     // public static final String TAGS_PROPERTY_KEY = "tags";
     public static final String DATE_PROPERTY_KEY = "date";
 
-    private Entity delegate;
+    private Entity entity;
 
-    public Lesson(Entity delegate) {
-        this.delegate = delegate;
+    public Lesson(Entity entity) {
+        this.entity = entity;
     }
 
     public Lesson(String title, String description) {
-        this.delegate = new Entity(Lesson.LESSON_ENTITY_NAME);
-        this.delegate.setProperty(Lesson.ISDRAFT_PROPERTY_KEY, false);
-        this.delegate.setProperty(User.NICKNAME_PROPERTY_KEY, title);
-        this.delegate.setProperty(User.USER_ID_PROPERTY_KEY, description);
+        this.entity = new Entity(Lesson.LESSON_ENTITY_NAME);
+        this.entity.setProperty(Lesson.ISDRAFT_PROPERTY_KEY, false);
+        this.entity.setProperty(Lesson.TITLE_PROPERTY_KEY, title);
+        this.entity.setProperty(Lesson.DESCRIPTION_PROPERTY_KEY, description);
+        this.entity.setProperty(Lesson.DATE_PROPERTY_KEY, new Date());
+    }
+
+    public Entity getLessonEntity() {
+        return this.entity;
+    }
+
+    public boolean getIsDraft() {
+        return (boolean) this.entity.getProperty(Lesson.ISDRAFT_PROPERTY_KEY);
+    }
+
+    public void publish() {
+        this.entity.setProperty(Lesson.ISDRAFT_PROPERTY_KEY, true);
+    }
+
+    public void unpublish() {
+        this.entity.setProperty(Lesson.ISDRAFT_PROPERTY_KEY, false);
+    }
+
+    public String getTitle() {
+        return (String) this.entity.getProperty(Lesson.TITLE_PROPERTY_KEY);
+    }
+
+    public void setTitle(String title) {
+        this.entity.setProperty(Lesson.TITLE_PROPERTY_KEY, title);
+    }
+
+    public String getDescription() {
+        return (String) this.entity.getProperty(Lesson.DESCRIPTION_PROPERTY_KEY);
+    }
+
+    public void setDescription(String description) {
+        this.entity.setProperty(Lesson.DESCRIPTION_PROPERTY_KEY, description);
     }
 
 }

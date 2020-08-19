767591bc71b7e7924d20bc32300a5671507432f3
Andrew Pham <0andrewpham0@gmail.com>
2020-08-19 20:03:43 +0000

    Create Class

---
+++ capstone/src/main/java/com/google/sps/data/RecommenderAlgorithm.java
@@ -0,0 +1,13 @@
+package com.google.sps.data;
+
+import com.google.sps.data.Room;
+import com.google.sps.data.User;
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.Entity;
+
+public class RecommenderAlgorithm {
+
+    public static List<Room> recommendRooms(User user){
+        
+    }
+}

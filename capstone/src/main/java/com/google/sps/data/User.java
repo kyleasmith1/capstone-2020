fda7bff7fc62d530823410cdf286ac69eafbca87
Kyle Smith <kysmith152@gmail.com>
2020-08-25 02:44:09 +0000

    Update user class

--- capstone/src/main/java/com/google/sps/data/User.java
+++ capstone/src/main/java/com/google/sps/data/User.java
@@ -2,11 +2,14 @@ package com.google.sps.data;
 
 import com.google.appengine.api.datastore.Key;
 import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.EmbeddedEntity;
+import com.google.sps.data.Tag;
 
 public class User {
     public static final String USER_ENTITY_NAME = "User";
     public static final String NICKNAME_PROPERTY_KEY = "nickname";
     public static final String USER_ID_PROPERTY_KEY = "userId";
+    public static final String TAGS_PROPERTY_KEY = "tags";
 
     private Entity delegate;
 
@@ -18,14 +21,23 @@ public class User {
         this.delegate = new Entity(User.USER_ENTITY_NAME);
         this.delegate.setProperty(User.NICKNAME_PROPERTY_KEY, nickname);
         this.delegate.setProperty(User.USER_ID_PROPERTY_KEY, id);
+        this.delegate.setProperty(User.TAGS_PROPERTY_KEY, Tag.constructUserEmbeddedEntity());
     }
 
-    public void changeNickname(String nickname) {
+    public void setEmbeddedTags(EmbeddedEntity entity) {
+        this.delegate.setProperty(User.TAGS_PROPERTY_KEY, entity);
+    }
+
+    public void setNickname(String nickname) {
         this.delegate.setProperty(User.NICKNAME_PROPERTY_KEY, nickname);
     }
 
     public String getId() {
         return (String) this.delegate.getProperty(User.USER_ID_PROPERTY_KEY);
+    }
+
+    public EmbeddedEntity getEmbeddedTags() {
+        return (EmbeddedEntity) this.delegate.getProperty(User.TAGS_PROPERTY_KEY);
     }
 
     public String getNickname() {
